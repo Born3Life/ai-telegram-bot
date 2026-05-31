d2f02d03d1be1a52a0ab978e88fed2c90b7aa5a0
Born3Life <122224313+Born3Life@users.noreply.github.com>
2026-05-31 19:51:13 +0200

    remove exam-bot (moved to separate repo shporyai)

--- exam-bot/bot/__main__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import asyncio
-
-from bot.main import main
-
-asyncio.run(main())
--- exam-bot/bot/handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from bot.handlers.start import router as start_router
-from bot.handlers.shpora import router as shpora_router
-
-routers = [shpora_router, start_router]
